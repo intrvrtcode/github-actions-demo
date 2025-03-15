b04f1f19e8c517e5956fcb91ac46f42b944c2b98
Aldi Rahmaddani <aldi.rahmaddani12@gmail.com>
2025-03-15 13:04:47 +0700

    feat: add multiply func

--- index.test.js
+++ index.test.js
@@ -7,3 +7,7 @@ test("adds 1 + 2 to equal 3", () => {
 test("subtracts 1 - 2 to equal -1", () => {
   expect(subtract(1, 2)).toBe(-1);
 });
+
+test("subtracts 2 - 1 to equal 1", () => {
+  expect(subtract(2, 1)).toBe(1);
+});
