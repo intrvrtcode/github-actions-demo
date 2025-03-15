29119531d88bdcfd1b7a2a8e935c8bf7996737e3
Aldi Rahmaddani <aldi.rahmaddani12@gmail.com>
2025-03-15 13:32:03 +0700

    feat: commit with squash merge

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const { add, subtract } = require("./index");
+const { add, subtract, multiply, divide } = require("./index");
 
 test("adds 1 + 2 to equal 3", () => {
   expect(add(1, 2)).toBe(3);
@@ -8,6 +8,10 @@ test("subtracts 1 - 2 to equal -1", () => {
   expect(subtract(1, 2)).toBe(-1);
 });
 
-test("subtracts 2 - 1 to equal 1", () => {
-  expect(subtract(2, 1)).toBe(1);
+test("multiplies 1 * 2 to equal 2", () => {
+  expect(multiply(1, 2)).toBe(2);
+});
+
+test("divides 1 / 2 to equal 0.5", () => {
+  expect(divide(1, 2)).toBe(0.5);
 });
