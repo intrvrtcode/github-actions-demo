45e3858abbef80e444a757ac020833887d6d2b5a
Aldi Rahmaddani <aldi.rahmaddani12@gmail.com>
2025-03-15 12:48:31 +0700

    feat: add subtrack func

--- index.test.js
+++ index.test.js
@@ -1,5 +1,9 @@
-const add = require("./index");
+const { add, subtract } = require("./index");
 
 test("adds 1 + 2 to equal 3", () => {
   expect(add(1, 2)).toBe(3);
+});
+
+test("subtracts 1 - 2 to equal -1", () => {
+  expect(subtract(1, 2)).toBe(-1);
 });
